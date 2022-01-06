1f26d3007ba9029e1e9ea1237eb9abcc1a019329
fgrunewald <f.grunewald@rug.nl>
2022-01-06 15:31:30 +0100

    actually average over all pairs

--- fast_forward/compute_bonded.py
+++ fast_forward/compute_bonded.py
@@ -5,7 +5,9 @@ import MDAnalysis as mda
 from fast_forward.bonded_functions import NORMAL_FUNCS
 
 def compute_value_for_interaction(universe, inter_type, valid_pairs):
-    for idxs in valid_pairs:
+    nframes = universe.trajectory.n_frames
+    time_series = np.zeros((len(valid_pairs) * nframes))
+    for idx, idxs in enumerate(valid_pairs):
         pair_pos = [ universe.trajectory.coordinate_array[:, pair, :] for pair in idxs]
-    time_series = NORMAL_FUNCS[inter_type](*pair_pos)
+        time_series[idx*nframes:(idx+1)*nframes] = NORMAL_FUNCS[inter_type](*pair_pos)
     return time_series
