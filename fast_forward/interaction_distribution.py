6c60f9c36e040a7864110d01fd74e908d0158f32
Chris Brasnett <35073246+csbrasnett@users.noreply.github.com>
2025-04-10 17:46:13 +0100

    add option to save interaction data files

--- fast_forward/interaction_distribution.py
+++ fast_forward/interaction_distribution.py
@@ -7,19 +7,21 @@ BINS_DICT = {"bonds": np.arange(0, 7, 0.01),
              "dihedrals": np.arange(-180, 181)
              }
 
-def interaction_distribution(u, inter_type, pair_idxs, group_name, prefix):
+def interaction_distribution(u, inter_type, pair_idxs, group_name, prefix, save):
     time_series = compute_value_for_interaction(u, inter_type, pair_idxs)
-    np.savetxt("{prefix}{name}_{inter_type}.dat".format(name=group_name,
-                                                        inter_type=inter_type,
-                                                        prefix=prefix),
-               time_series)
+    if save:
+        np.savetxt("{prefix}{name}_{inter_type}.dat".format(name=group_name,
+                                                            inter_type=inter_type,
+                                                            prefix=prefix),
+                   time_series)
 
     probs, edges = np.histogram(time_series, density=True, bins=BINS_DICT[inter_type])
     center_points = edges[:-1] + np.diff(edges)/2.
     distr = np.transpose((center_points, probs))
-    np.savetxt("{prefix}{name}_{inter_type}_distr.dat".format(name=group_name,
-                                                              inter_type=inter_type,
-                                                              prefix=prefix),
-               distr)
+    if save:
+        np.savetxt("{prefix}{name}_{inter_type}_distr.dat".format(name=group_name,
+                                                                  inter_type=inter_type,
+                                                                  prefix=prefix),
+                   distr)
 
     return distr
