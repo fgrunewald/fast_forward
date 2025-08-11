fc64500244923eb9cb3a357995ee6eeea1f0892c
csbrasnett <c.s.brasnett@rug.nl>
2025-08-11 10:01:28 +0200

    fix virutal site mass reassignment if the mass has already manually been reassigned

--- fast_forward/virtual_site_mass.py
+++ fast_forward/virtual_site_mass.py
@@ -1,5 +1,7 @@
 
-MARTINI_MASS=  {"S": 54, "T": 36}
+from numpy import isclose
+
+MARTINI_MASS = {"S": 54, "T": 36}
 
 def mass_redistribution(block, pair_idx):
     """
@@ -20,8 +22,19 @@ def mass_redistribution(block, pair_idx):
         redistribution = vs_type_mass / len(pairs[1:])
 
         # if only 1 constructor (i.e. building vs directly on top of atom), no redistribution necessary.
+        reassign = []
         if len(pairs[1:]) > 1:
             constructors = pairs[1:]
             for node in constructors:
                 current_mass = block.nodes[node].get("mass", MARTINI_MASS.get(block.nodes[node]["atype"][0], 72))
-                block.nodes[node]["mass"] = current_mass + redistribution
+                expected_new_mass = MARTINI_MASS.get(block.nodes[node]["atype"][0], 72) + redistribution
+
+                if not isclose(current_mass, expected_new_mass):
+                    block.nodes[node]["mass"] = expected_new_mass
+                else:
+                    reassign.append(True)
+
+        if any(reassign):
+            print(f"Masses for constructor beads of virtual site at index {vs+1} "
+                  f"did not correspond to standard Martini masses. Will not attempt "
+                  f"automatic mass reassignment.")
